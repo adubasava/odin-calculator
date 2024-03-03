6d05ccbc5abd01172aece0e230ba764b847c929e
adubasava <anzhalikad@gmail.com>
2024-03-03 15:15:27 +0300

    Add calculator functions (not finished)

--- script.js
+++ script.js
@@ -19,28 +19,26 @@ let displayValue;
 let firstNumber;
 let secondNumber;
 let operator;
+let eq;
+let last = 0;
+let next;
 
 function operate(operator, num1, num2) {
     switch(operator) {
         case '+':
-            displayValue = add(num1, num2);
-            console.log(displayValue);
-            break;
+            displayValue = add(num1, num2);   
+            return displayValue;
         case '-':
             displayValue = substract(num1, num2);
-            console.log(displayValue);
-            break;
+            return displayValue;
         case '*':
             displayValue = multiply(num1, num2);
-            console.log(displayValue);
-            break;
+            return displayValue;
         case '/':
             displayValue =  divide(num1, num2);
-            console.log(displayValue);
-            break;
+            return displayValue;
     }    
 }
-
 
 const display = document.querySelector('.results');
 
@@ -48,46 +46,87 @@ const numButtons = document.querySelectorAll('.number');
 
 numButtons.forEach((button) => {
     button.addEventListener('click', () => {
-        if (document.querySelector('.results').textContent == 0) {
-            document.querySelector('.results').textContent = '';
-        }  
-        if (operator) {
-            secondNumber = document.querySelector('.results').textContent;
-            document.querySelector('.results').textContent = '';
-            operator = null;
+        if (last == 0) {
+            display.textContent = '' + button.textContent;
+        } else if (last == operator || last == eq) {
+            display.textContent = button.textContent;
+        } else {
+            display.textContent += button.textContent;
         }
-        document.querySelector('.results').textContent += button.textContent;
-        console.log(display.textContent);
-        });
+        
+        last = button.textContent;
+    });
 });
+
+function handleNumbers(num) {
+    if (!firstNumber) {
+        firstNumber = num;
+        return firstNumber;
+    } else {
+        secondNumber = num;
+        return secondNumber;
+    }
+}
 
 const clear = document.querySelector('.clear');
 clear.addEventListener('click', () => {
-    document.querySelector('.results').textContent = 0;
+    display.textContent = 0;
+    last = 0;
+    firstNumber = null;
+    secondNumber = null;
+    operator = null;
 })
 
 const oper = document.querySelectorAll('.operator');
 
 oper.forEach((oper) => {
-    oper.addEventListener('click', () => {        
-        operator = oper.textContent;
-        firstNumber = display.textContent;
-        document.querySelector('.results').textContent = firstNumber;
+    oper.addEventListener('click', () => {
+        if (!operator) {
+            operator = oper.textContent;
+            last = operator;
+        } else {
+            next = oper.textContent;
+            last = next;
+        }
+        
+        handleNumbers(Number(display.textContent));
+        if (firstNumber && secondNumber && operator) {
+            display.textContent = operate(operator, firstNumber, secondNumber);
+            firstNumber = display.textContent;
+            operator = next;
+            secondNumber = null;
+        }
     })
 });
 
 const equal = document.querySelector('.equal');
 equal.addEventListener('click', () => {
-    if (firstNumber && secondNumber && operator) {
-        document.querySelector('.results').textContent = operate(operator, firstNumber, secondNumber);
+    eq = equal.textContent;
+    
+    secondNumber = handleNumbers(Number(display.textContent));
+    if (firstNumber && secondNumber && operator && last != operator) {
+        display.textContent = operate(operator, firstNumber, secondNumber);
+        firstNumber = display.textContent;
+        secondNumber = null;
     } else {
-        display.textContent = 0;
+        firstNumber = Number(display.textContent);
+        secondNumber = null;
     }    
+    last = eq;
 })
 
 const back = document.querySelector('.back');
 back.addEventListener('click', () => {
     display.textContent = display.textContent.slice(0,display.textContent.length-1);
+})
+
+const sign = document.querySelector('.sign');
+sign.addEventListener('click', () => {
+    if (display.textContent[0] == "-") {
+        display.textContent = display.textContent.slice(1,display.textContent.length);
+    } else {
+        display.textContent = "-" + display.textContent;
+    }
 })
 
 
