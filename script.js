f14db72efaeac8661996c9c875371699ca89ee8f
adubasava <anzhalikad@gmail.com>
2024-03-04 19:20:51 +0300

    Add precision to operations with float numbers

--- script.js
+++ script.js
@@ -54,6 +54,10 @@ function clearAll() {
     last = firstNumber = secondNumber = operator = null;
 }
 
+function roundFloat(num) {
+    return parseFloat(Math.round(num + 'e' + 15) + 'e-' + 15);
+}
+
 const numButtons = document.querySelectorAll('.number');
 numButtons.forEach((button) => {
     button.addEventListener('click', () => {
@@ -61,8 +65,7 @@ numButtons.forEach((button) => {
             display.textContent += button.textContent;            
         } else if (last == 0 && display.textContent.length == 1) {
             display.textContent = '' + button.textContent;            
-        } 
-        else if (last == "=") {
+        } else if (last == "=") {
             clearAll();
             display.textContent = button.textContent;
         } else if (last == operator || last == "sqrt") {
@@ -82,16 +85,17 @@ clear.addEventListener('click', () => {
 
 const oper = document.querySelectorAll('.operator');
 oper.forEach((oper) => {
-    oper.addEventListener('click', () => {
+    oper.addEventListener('click', () => {    
+
+        handleNumbers(Number(display.textContent));
+
         if (!operator) {
             operator = oper.textContent;
         } else {
             next = oper.textContent;
-        }
+        }    
         
-        handleNumbers(Number(display.textContent));
-
-        if (display.textContent == "Oops!") {
+        if (display.textContent == "Oops!" || firstNumber === null) {
             clearAll();
         } else if (last == "=" || last == "sqrt") {
             firstNumber = Number(display.textContent);
@@ -101,12 +105,13 @@ oper.forEach((oper) => {
         } else if (firstNumber !== null && secondNumber !== null) {
             display.textContent = operate(operator, firstNumber, secondNumber);
             if (display.textContent == "Oops!") {            
-                firstNumber = null;
+                firstNumber = secondNumber = operator = null;
             } else {
+                display.textContent = roundFloat(display.textContent);
                 firstNumber = Number(display.textContent);
-            }             
-            operator = next;
-            secondNumber = null;
+                operator = next;
+                secondNumber = null;
+            } 
         } 
 
         last = operator;
@@ -119,17 +124,19 @@ equal.addEventListener('click', () => {
     handleNumbers(Number(display.textContent));
 
     if (firstNumber !== null && secondNumber !== null && operator && last != operator) {
+        firstNumber = Number(firstNumber);
+        secondNumber = Number(secondNumber);
         display.textContent = operate(operator, firstNumber, secondNumber);
         if (display.textContent == "Oops!") {            
-            firstNumber = null;
+            last = firstNumber = secondNumber = operator = null;
         } else {
+            display.textContent = roundFloat(display.textContent);
             firstNumber = Number(display.textContent);
-        }        
-        secondNumber = null;
-        operator = null;
+            secondNumber = operator = null;
+        }
     } else {
-        firstNumber = display.textContent;
-        secondNumber = null;
+        firstNumber = Number(display.textContent);
+        secondNumber = null;        
     }    
     last = "=";
 })
@@ -144,28 +151,31 @@ back.addEventListener('click', () => {
 
 const sqrt = document.querySelector('.sqrt');
 sqrt.addEventListener('click', () => {    
+
     let num = Number(display.textContent);
     if (num >= 0) {
-        display.textContent = Math.sqrt(num);
-        firstNumber = Number(display.textContent);
+        display.textContent = firstNumber = Math.sqrt(num);
+        secondNumber = operator = null;
     } else {
         display.textContent = "Oops!";
         last = firstNumber = secondNumber = operator = null;
     }
-        
-    secondNumber = null;
-    last = "sqrt";
+    
+    last = 'sqrt';
 })
 
 const sign = document.querySelector('.sign');
 sign.addEventListener('click', () => {
     if (display.textContent == 0) {
         display.textContent = 0;
+        firstNumber = Number(display.textContent);
     } else if (display.textContent[0] == "-") {
         display.textContent = display.textContent.slice(1,display.textContent.length);
+        firstNumber = Number(display.textContent);
     } else {
         display.textContent = "-" + display.textContent;
-    }
+        firstNumber = Number(display.textContent);
+    }    
 })
 
 const decimal = document.querySelector('.decimal');
@@ -178,9 +188,9 @@ decimal.addEventListener('click', () => {
     } else if (last == "=" || last == "sqrt") {
         clearAll();
         display.textContent = 0 + ".";
+        last = ".";
     } else if (!display.textContent.includes(".")) {
         display.textContent += ".";
         last = ".";
     }       
 });
-
